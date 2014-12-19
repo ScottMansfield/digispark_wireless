df498b02e2d0fef1cccd065ad510f2240b48af2b
Scott Mansfield <sgmansf@gmail.com>
2014-12-19 14:33:00 -0800

    Adding some work-in-progress functions

--- digispark_wireless.ino
+++ digispark_wireless.ino
@@ -10,12 +10,26 @@
 
 void setup_NRF24L01P()
 {
+  // NRF24L01+ needs 100ms for both power on ramp up
+  // (max) and the power on reset (max), so wait 200ms
+  delay(200);
+  
+  // set configuration here
+  
+  // set PWR_UP to 1 to move to standby
+  // The chip enable pin is tied to high,
+  //  so it immediately moves to standby-II
   // lol
+  
+  // wait 5 ms for crystal to stabilize
+  delay(5);
 }
 
 // SPI transfer, MSB first
 uint8_t spi_transfer(uint8_t data)
 {
+  digitalWrite(CSN_PIN, LOW);
+  
   USIDR = data;
   USISR = _BV(USIOIF); // clear flag
 
@@ -33,6 +47,9 @@ void setup()
   pinMode(MOSI_PIN, OUTPUT);
   pinMode(SCLK_PIN, OUTPUT);
   pinMode(CSN_PIN,  OUTPUT);
+  
+  // SPI chip select is active-low
+  digitalWrite(CSN_PIN, HIGH);
   
   setup_NRF24L01P();
 }
